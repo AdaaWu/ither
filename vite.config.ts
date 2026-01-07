8293fcd212d2f590b7ee1e61a5fb5d61549ee998
Sunny Wu <idonga500@gmail.com>
2026-01-07 16:58:18 +0800

    設定 Github Page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/vue-it-woman/',  // GitHub repo 名稱
   plugins: [vue()],
   resolve: {
     alias: {
